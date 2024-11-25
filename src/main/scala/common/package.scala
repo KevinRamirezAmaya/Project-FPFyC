895a585d424f48e65759bedbe68cc5fe4dbeb016
SergioART2023 <sergio.rodriguez.trujillo@correounivalle.edu.co>
2024-11-24 22:17:52 -0500

    First test commit

--- src/main/scala/common/package.scala
+++ src/main/scala/common/package.scala
@@ -3,7 +3,7 @@ import scala.util.DynamicVariable
 
 package object common {
   val forkJoinPool = new ForkJoinPool
-
+//abstract class
   abstract class TaskScheduler {
     def schedule[T](body: => T): ForkJoinTask[T]
     def parallel[A, B](taskA: => A, taskB: => B): (A, B) = {
